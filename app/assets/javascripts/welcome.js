3515bbeaab09cf9ced2f896a2abba72d8033b2cd
Spencer Christopherson <schristopherson@gmail.com>
2015-12-16 20:40:53 -0700

    working game

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,122 @@
+$(document).ready(function(){
+
+	var firstNumberClicked;
+	var secondNumberClicked;
+	var ticker =0;
+	var matchesFoundTicker = 0;
+
+	var myArray = ['1','1','1','1','2','2','2',
+			'2','3','3','3','3','4','4','4','4'];
+
+	//for testing completed
+	// var myArray = ['1','1','1','1','1','1','1',
+	// 		'1','1','1','1','1','1','1','1','1'];
+
+	populateBoard();
+
+	function shuffle(o){
+    for(var j, x, i = o.length; i; j = 
+    Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+	}
+
+	function populateBoard(){
+		shuffle(myArray);
+		$('.boxes').each(function(){
+			$(this).children(0).html(myArray.pop());
+			$(this).children(0).addClass('hide_text');
+			$(this).children(0).removeClass('matched');
+
+		})
+		matchesFoundTicker = 0;
+	}
+
+	function clickTracker(){
+		if (ticker <2) {
+			ticker++;
+		} else{
+			ticker = 0;
+		}
+		console.log("clickTracker:" + ticker);
+	};
+
+	//check if box has already been clicked
+	function isShowing(number_class){
+		if (number_class != 'hide_text') {
+			alert("Is showing");
+			return false;
+
+		}else{
+			return true;
+		}
+	}
+
+	function checkCompleted(){
+		if (matchesFoundTicker >= 8) {
+			alert('You Win!!!!');
+			populateBoard();
+		};
+
+	}
+
+	function checkMatch(num1,num2){
+		if (num1 == num2) {
+		
+				$('.boxes').each(function(){
+					var currentClass = $(this).children(0).attr('class');
+					if (currentClass != 'hide_text') {
+						$(this).children(0).addClass('matched');
+					};
+
+				});
+				
+				alert("match");
+				//this should be moved
+				clickTracker();
+				matchesFoundTicker++;
+				return true;
+
+		} else{
+
+				alert("Not A Match");
+
+			$('.boxes').each(function(){
+					var currentClass = $(this).children(0).attr('class');
+					if (currentClass != 'hide_text' && currentClass != 'matched') {
+						$(this).children(0).addClass('hide_text');
+					};
+
+				});
+				//this should be moved
+				clickTracker();
+				return false;
+		};
+	}
+
+
+
+	$('.boxes').click(function(){
+
+		var currentClass = $(this).children(0).attr('class');
+			if (isShowing(currentClass) ){
+				$(this).children(0).toggleClass('hide_text');
+				clickTracker();
+
+			};
+
+		 if (ticker == 1) {
+		 	firstNumberClicked = $(this).children(0).html();
+		};
+
+		if (ticker == 2) {
+		 	secondNumberClicked = $(this).children(0).html();
+
+		 	checkMatch(firstNumberClicked, secondNumberClicked);
+		 	checkCompleted();
+
+		};
+
+	})
+
+});
+
